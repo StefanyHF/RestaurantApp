c3380a884f1911b3496fc71ad27483c29bdaf566
Stefany <https://github.com/StefanyHF/AndroidCourse.git>
2021-03-01 22:02:54 +0100

    Restaurant app

---
+++ app/src/main/java/com/exercises/restaurantapp/MainActivity.java
@@ -0,0 +1,61 @@
+package com.exercises.restaurantapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    CardView startersCard;
+    CardView mainsCard;
+    CardView dessertsCard;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        startersCard = findViewById(R.id.card_view_starters);
+        mainsCard = findViewById(R.id.card_view_main);
+        dessertsCard = findViewById(R.id.card_view_desserts);
+
+        startersCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startActivityIntent = new Intent(MainActivity.this, StartersActivity.class );
+                startActivity(startActivityIntent);
+            }
+        });
+
+        mainsCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mainCoursesIntent = new Intent(MainActivity.this, MainCoursesActivity.class);
+                startActivity(mainCoursesIntent);
+            }
+        });
+        TextView emailTextView = findViewById(R.id.text_view_email_address);
+        emailTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent launchEmailAppIntent = new Intent(Intent.ACTION_SENDTO);
+                launchEmailAppIntent.setData(Uri.parse("mailto:restaurantapp@app.com"));
+                startActivity(launchEmailAppIntent);
+            }
+        });
+        dessertsCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent dessertsIntent = new Intent(MainActivity.this, DessertsActivity.class);
+                startActivity(dessertsIntent);
+            }
+        });
+    }
+
+
+
+}
